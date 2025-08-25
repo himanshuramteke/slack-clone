ecc4d675fddbbc2725d4e65f49cf50618a0c15b5
Himanshu Ramteke <himanshuramteke007@gmail.com>
2025-08-25 21:53:29 +0530

    initial commit

---
+++ backend/src/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { PORT } from "./config/serverConfig.js";
+
+const app = express();
+
+app.get("/ping", (req, res) => {
+  return res.json({ message: "Pong" });
+});
+
+app.listen(PORT, () => {
+  console.log("Server is running on PORT: 5001");
+});
