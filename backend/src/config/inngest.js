5e97c01d69319db3d297724f484c723c5d5b0ad8
Himanshu Ramteke <himanshuramteke007@gmail.com>
2025-08-26 17:49:14 +0530

    Added small fix in inngest.js

--- backend/src/config/inngest.js
+++ backend/src/config/inngest.js
@@ -1,5 +1,6 @@
 import { Inngest } from "inngest";
 import { connectDB } from "./db.js";
+import { User } from "../models/user.model.js";
 
 export const inngest = new Inngest({ id: "slack-clone" });
 
@@ -28,6 +29,7 @@ const deleteUserFromDB = inngest.createFunction(
   { id: "delete-user-from-db" },
   { event: "clerk/user.deleted" },
   async ({ event }) => {
+    await connectDB();
     const { id } = event.data;
     await User.deleteOne({ clerkId: id });
     await deleteStreamUser(id.toString());
