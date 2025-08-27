638ebaa906f6426190055bbe9a60b20ea0c00bb2
Himanshu Ramteke <himanshuramteke007@gmail.com>
2025-08-27 17:41:46 +0530

    Added stream and sentry

--- backend/src/config/inngest.js
+++ backend/src/config/inngest.js
@@ -1,6 +1,7 @@
 import { Inngest } from "inngest";
 import { connectDB } from "./db.js";
 import { User } from "../models/user.model.js";
+import { deleteStreamUser, upsertStreamUser } from "./stream.js";
 
 export const inngest = new Inngest({ id: "slack-clone" });
 
@@ -21,7 +22,11 @@ const syncUser = inngest.createFunction(
     };
     await User.create(newUser);
 
-    //Todo:
+    await upsertStreamUser({
+      id: newUser.clerkId.toString(),
+      name: newUser.name,
+      image: newUser.image,
+    });
   }
 );
 
@@ -33,7 +38,8 @@ const deleteUserFromDB = inngest.createFunction(
     const { id } = event.data;
     await User.deleteOne({ clerkId: id });
     await deleteStreamUser(id.toString());
-    //todo:
+
+    await deleteStreamUser(id.toString());
   }
 );
 
---
+++ backend/src/config/stream.js
@@ -0,0 +1,33 @@
+import { StreamChat } from "stream-chat";
+import { STREAM_API_KEY, STREAM_SECRET_KEY } from "./serverConfig.js";
+
+const streamClient = StreamChat.getInstance(STREAM_API_KEY, STREAM_SECRET_KEY);
+
+export const upsertStreamUser = async (userData) => {
+  try {
+    await streamClient.upsertUser(userData);
+    console.log("Stream user upserted successfully", userData.name);
+    return userData;
+  } catch (error) {
+    console.log("Error upserting Stream user:", error);
+  }
+};
+
+export const deleteStreamUser = async (userId) => {
+  try {
+    await streamClient.deleteUser(userId);
+    console.log("Stream user deleted successfully", userId);
+  } catch (error) {
+    console.log("Error deleting Stream user:", error);
+  }
+};
+
+export const generateStreamToken = (userId) => {
+  try {
+    const userIdString = userId.toString();
+    return streamClient.createToken(userIdString);
+  } catch (error) {
+    console.log("Error generating Stream Token", error);
+    return null;
+  }
+};
